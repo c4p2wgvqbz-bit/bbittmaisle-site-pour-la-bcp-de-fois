a379078920c0785db314e162ef1eb472a469e900
c4p2wgvqbz-bit <c4p2wgvqbz@privaterelay.appleid.com>
2026-01-19 22:32:33 +0100

    Improve detailHtml for version 1.1
    Updated HTML content in the detail section for clarity.

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
       version: '1.1',
       date: '07-02-2026',
       teaser: 'Cette mise à jour apporte plusieurs améliorations importantes pour rendre l’expérience plus claire, plus moderne et plus agréable à utiliser.',
-      detailHtml: `<p>Amélioration de la rubrique Nouveautés, avec un affichage plus clair des versions. Correction du badge « 1 », qui disparaît désormais lorsqu’il est consulté. Ajout d’un bouton "Suivi du jeu" dans la section "Brad Bitt, mais le jeu" pour accéder directement au développement du projet. Optimisation générale de l’interface sur ordinateur et console.</p>`
+      detailHtml: `<p>Amélioration de la rubrique "Nouveautés", avec un affichage plus clair des versions. Correction du badge « 1 », qui disparaît désormais lorsqu’il est consulté. Ajout d’un bouton "Suivi du jeu" dans la section "Brad Bitt, mais le jeu" pour accéder directement au développement du projet. Optimisation générale de l’interface sur ordinateur et console.</p>`
     },
     {
       version: '1.0',
